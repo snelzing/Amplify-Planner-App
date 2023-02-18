72402429e25cbce12dd40e77a8cf152a1cdafa24
Shelby <39928924+snelzing@users.noreply.github.com>
2023-02-18 02:03:19 -0500

    intl

---
+++ README.md
@@ -0,0 +1,7 @@
+# Point of this Repo
+
+To learn how to use AWS amplify with Flutter, as a PoC for Sidewalk SideKick
+
+## The tutorial I am following:
+
+https://aws.amazon.com/getting-started/hands-on/build-flutter-mobile-app-part-one/ 
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:amplify_trips_planner/trips_planner_app.dart';
+import 'package:flutter/material.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'amplifyconfiguration.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  bool isAmplifySuccessfullyConfigured = false;
+  try {
+    await _configureAmplify();
+    isAmplifySuccessfullyConfigured = true;
+  } on AmplifyAlreadyConfiguredException {
+    debugPrint('Amplify configuration failed.');
+  }
+
+  runApp(
+    ProviderScope(
+      child: TripsPlannerApp(
+        isAmplifySuccessfullyConfigured: isAmplifySuccessfullyConfigured,
+      ),
+    ),
+  );
+}
+
+Future<void> _configureAmplify() async {
+  await Amplify.addPlugins([
+    AmplifyAuthCognito(),
+  ]);
+  await Amplify.configure(amplifyconfig);
+}
